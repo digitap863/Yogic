51ba88c16aad721df07422bef306b873a485ac1b
feminafiroz <feminafiroz123@gmail.com>
2024-09-26 17:42:39 +0530

    link changed

--- client/src/api/url.js
+++ client/src/api/url.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const url =import.meta.env.MODE === "development"?'http://localhost:3000/api':'https://snescollege.ac.in/api'
+const url =import.meta.env.MODE === "development"?'http://localhost:3000/api':'https://yogic-1.onrender.com'
 export const AxiosAdmin = axios.create({
     baseURL:url
 })
