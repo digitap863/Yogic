d759109910001ecd47b9487d9479ddf10d2c1e47
feminafiroz <feminafiroz123@gmail.com>
2025-07-03 12:30:19 +0530

    backend new url added

--- client/src/api/url.js
+++ client/src/api/url.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const url =import.meta.env.MODE === "development"?'http://localhost:3000/api':'https://yogic-1.onrender.com/api'
+export const url =import.meta.env.MODE === "development"?'http://localhost:3000/api':'https://yogic.onrender.com//api'
 export const AxiosAdmin = axios.create({
     baseURL:url
 })
